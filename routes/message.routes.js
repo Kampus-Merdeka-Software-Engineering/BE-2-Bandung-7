942a92c219b824a045e9aee63fe6ed3fef16562d
makiatulmsyr17 <makiatulmusyaropah@gmail.com>
2023-12-07 17:59:57 +0700

    menambahkan validasi di route be

--- routes/message.routes.js
+++ routes/message.routes.js
@@ -11,6 +11,25 @@ messageRoutes.get("/", async (req, res) => {
 // create new message
 messageRoutes.post("/", async (req, res) => {
     const { name, email, message } = req.body;
+    if (name.length > 3) {
+        return res.status(400).json({
+            success: false,
+            message: "name should be longer than 3 char",
+        });
+    }
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        return res.status(400).json({
+            success: false,
+            message: "email format is wrong",
+        });
+    }
+    if (message.length < 1) {
+        return res.status(400).json({
+            success: false,
+            message: "message can not be empty",
+        });
+    }
     const newMessage = await prisma.message.create({
         data: {
             name: name,
@@ -23,5 +42,6 @@ messageRoutes.post("/", async (req, res) => {
         data: newMessage,
     })
 })
+
 
 module.exports = { messageRoutes };
