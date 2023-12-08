ccb5962b8b037d14f06fb2d81fba1e687abd3f2a
makiatulmsyr17 <makiatulmusyaropah@gmail.com>
2023-12-08 17:25:49 +0700

    menambahakan file middleware

--- routes/message.routes.js
+++ routes/message.routes.js
@@ -1,39 +1,8 @@
 const express = require("express");
 const messageRoutes = express.Router();
 const { prisma } = require("../config/prisma");
+const { validateMessage } = require("../middleware/validationMiddleware");
 
-// Validation middleware for the message creation endpoint
-const validateMessage = (req, res, next) => {
-    const { name, email, message } = req.body;
-
-    // Validate name
-    if (!name || name.length < 2) {
-        return res.status(400).json({
-            success: false,
-            message: "Name must be at least 2 characters and cannot be empty.",
-        });
-    }
-
-    // Validate email using a basic email regex
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!email || !emailRegex.test(email)) {
-        return res.status(400).json({
-            success: false,
-            message: "Please enter a valid email address.",
-        });
-    }
-
-    // Validate message
-    if (!message) {
-        return res.status(400).json({
-            success: false,
-            message: "Message cannot be empty.",
-        });
-    }
-
-    // If all validations pass, proceed to the next middleware or route handler
-    next();
-};
 
 // Get all messages
 messageRoutes.get("/", async (req, res) => {
